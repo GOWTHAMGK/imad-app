42959d4a1c197655f84d8489a7cbee1e82b20117
GOWTHAMGK <31180606+GOWTHAMGK@users.noreply.github.com>
2017-08-23 16:18:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,18 @@
 //counter
 var button = document.getElementById('counter');
-var counter =0;
 
 button.onclick = function() {
-counter = counter + 1;
-var span = document.getElementById('count');
-span.innerHTML = counter.toString();
+    var request = new XMLhttpRequest();
+    
+    request.onreadystatechange = function() {
+        
+       if(request.readystate === XMLhttpRequest.DONE) {
+           
+           if(request.status === 200) {
+               var counter =request.responceText;
+               var span = document.getElementById('count');
+               span.innerHTML = counter.toString();
+           }
+       } 
+    };
 };
