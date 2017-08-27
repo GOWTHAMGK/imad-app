298b605b582e153f689e163f39e58b6710616bb5
GOWTHAMGK <31180606+GOWTHAMGK@users.noreply.github.com>
2017-08-27 22:35:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var pool = new pool(config);
 app.get('/text-db', function (req, res){
     pool.quary('SELECT* from text',function (req, res){
     if(err){
-        res.status(500),send(err.toString());
+        res.status(500).send(err.toString());
     }else{
         res.send(JSON.stringfy(result));
     }
